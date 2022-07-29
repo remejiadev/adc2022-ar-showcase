7be1e7ad4235a12c40acb1843e5597c03928dc46
Rodrigo Mejia <109994165+remejiadev@users.noreply.github.com>
2022-07-29 14:16:05 -0600

    :sparkles: Add ArSphere view template

---
+++ adc_ar_showcase/adc_ar_showcase/modules/sphere_showcase/View/ARSphere.swift
@@ -0,0 +1,20 @@
+//
+//  ARSphere.swift
+//  adc_ar_showcase
+//
+//  Created by APPLAUDO on 29/7/22.
+//
+
+import SwiftUI
+
+struct ARSphere: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ARSphere_Previews: PreviewProvider {
+    static var previews: some View {
+        ARSphere()
+    }
+}
