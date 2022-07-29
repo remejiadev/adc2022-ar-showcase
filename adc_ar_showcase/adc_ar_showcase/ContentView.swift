a7519166462e697707f0b43c35cc8b1ad848a9b6
Rodrigo Mejia <109994165+remejiadev@users.noreply.github.com>
2022-07-29 10:03:13 -0600

    :coffin: Remove reference to deleted asset

--- adc_ar_showcase/adc_ar_showcase/ContentView.swift
+++ adc_ar_showcase/adc_ar_showcase/ContentView.swift
@@ -19,12 +19,7 @@ struct ARViewContainer: UIViewRepresentable {
     func makeUIView(context: Context) -> ARView {
         
         let arView = ARView(frame: .zero)
-        
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+
         
         return arView
         
