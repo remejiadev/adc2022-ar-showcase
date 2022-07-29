d013918496f9a9e4e71ceea50772147666a4cd1e
Rodrigo Mejia <109994165+remejiadev@users.noreply.github.com>
2022-07-29 11:55:40 -0600

    :sparkles: Add coaching overlay extension to ARView

---
+++ adc_ar_showcase/adc_ar_showcase/extensions/ARView+CoachingOverlay.swift
@@ -0,0 +1,23 @@
+//
+//  ARView+CoachingOverlay.swift
+//  adc_ar_showcase
+//
+//  Created by APPLAUDO on 29/7/22.
+//
+
+import Foundation
+import RealityKit
+import ARKit
+
+extension ARView: ARCoachingOverlayViewDelegate {
+    func addCoaching() {
+        let coachingOverlay = ARCoachingOverlayView()
+        coachingOverlay.delegate = self
+        #if !targetEnvironment(simulator)
+        coachingOverlay.session = self.session
+        #endif
+        coachingOverlay.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        coachingOverlay.goal = .horizontalPlane
+        self.addSubview(coachingOverlay)
+    }
+}
