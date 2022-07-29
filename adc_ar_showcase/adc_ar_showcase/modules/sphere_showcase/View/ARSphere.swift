b9e0326aaea981357ed860cfa35035f6f8a2f34c
Rodrigo Mejia <109994165+remejiadev@users.noreply.github.com>
2022-07-29 15:01:08 -0600

    :lipstick: Add Sphere view representable to swiftui container

--- adc_ar_showcase/adc_ar_showcase/modules/sphere_showcase/View/ARSphere.swift
+++ adc_ar_showcase/adc_ar_showcase/modules/sphere_showcase/View/ARSphere.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ARSphere: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ARSphereView().edgesIgnoringSafeArea(.all)
     }
 }
 
