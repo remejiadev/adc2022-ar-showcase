2866316dcc0c35a364870608faaba16791ec33f6
Rodrigo Mejia <109994165+remejiadev@users.noreply.github.com>
2022-07-29 14:16:37 -0600

    :art: Add new case to showcases
    Also add destination computed var

--- adc_ar_showcase/adc_ar_showcase/modules/home/Model/Showcase.swift
+++ adc_ar_showcase/adc_ar_showcase/modules/home/Model/Showcase.swift
@@ -6,10 +6,25 @@
 //
 
 import Foundation
+import SwiftUI
 
 enum ShowCase: String, CaseIterable, Identifiable{
     
     var id : String { UUID().uuidString }
     
     case applaudoCar = "AR Car"
+    case arSphere = "AR Sphere"
+}
+
+// MARK: - ShowCase View
+
+extension ShowCase {
+    var destination: some View {
+        switch self {
+        case .applaudoCar:
+            return AnyView(ARCar())
+        case .arSphere:
+            return AnyView(ARSphere())
+        }
+    }
 }
